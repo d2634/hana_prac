1d63c3d55c9cdaec39143cc895e8b00a5e4dbba2
d2634 <d2634@naver.com>
2024-02-18 20:57:01 +0900

    주말동안 한거

---
+++ app/src/main/java/com/example/dahee_prac/model/MainData.kt
@@ -0,0 +1,6 @@
+package com.example.dahee_prac.model
+
+class MainData (
+    var title : String,
+    var img : Int
+)
--- app/src/main/java/com/example/dahee_prac/view/MainActivity.kt
+++ app/src/main/java/com/example/dahee_prac/view/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.dahee_prac.view
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.dahee_prac.R
 import com.example.dahee_prac.databinding.MainActivityBinding
+import com.example.dahee_prac.model.MainData
 
 
 class MainActivity : Activity() {
@@ -13,9 +16,17 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = MainActivityBinding.inflate(layoutInflater)
+        /*binding = MainActivityBinding.inflate(layoutInflater)
         val view = binding.root
-        setContentView(view)
+        setContentView(view)*/
+
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.main_activity
+        )
+
+        val setdata = MainData("제목", R.drawable.ic_launcher_background)
+        binding.maindata=setdata
 
         val intent = Intent(this, SecondActivity::class.java)
         intent.putExtra("data1","텍스트를 넘겨요~")
